f337e3d8353709fa37a12b70a49debeb7743646e
mankuBadal24 <144877956+mankuBadal24@users.noreply.github.com>
2024-11-26 23:30:32 +0530

    login Api update

--- src/main/java/com/app/service/UserService.java
+++ src/main/java/com/app/service/UserService.java
@@ -4,7 +4,9 @@ import com.app.model.FarmerRegisDetails;
 import com.app.model.UserDtls;
 import com.app.repository.UserRepository;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -24,13 +26,19 @@ public class UserService {
         userRepository.saveFarmerDetail(farmer);
     }
 
-    public UserDtls login(String phoneNumber, String password) {
-        UserDtls user = userRepository.findByPhoneNumber(phoneNumber);
-        if (user != null && user.getPassword().equals(password)) {
-            return user; 
-        }
-        return null; 
+    public Map<String, Object> login(String phoneNumber, String password) {
+    UserDtls user = userRepository.findByPhoneNumber(phoneNumber);
+    if (user != null && user.getPassword().equals(password)) {
+        
+        Map<String, Object> userData = new LinkedHashMap<>();
+        userData.put("id", user.getId());
+        userData.put("name", user.getName());
+        userData.put("role", user.getRole());
+        return userData;
     }
+    return null; 
+}
+
     public List<String> getFarmerNamesByCropCode(String cropCode) {
         return userRepository.findFarmerNamesByCropCode(cropCode);
     }
